fc60a5eedb4d86d5577f9f87a6c995fde3192e68
whoami <128888679+jahangirsohag@users.noreply.github.com>
2025-03-18 03:48:19 +0600

    Segment Tree Lazy.cpp

--- Segment Tree Lazy.cpp
+++ Segment Tree Lazy.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
+#define N 100000
+#define ll long long
+
 using namespace std;
 
-const int N = 100000;
-
-struct ST {
-	long long sum;
-	long long prop;
+struct St {
+	ll sum;
+	ll prop;
 } tree[4*N];
 
 void build(int n, int b, int e)
@@ -25,16 +26,15 @@ void build(int n, int b, int e)
 	build(r, mid+1, e);
 
 	tree[n].sum = tree[l].sum + tree[r].sum;
-	tree[n].prop = 0;
 }
 
-void update(int n, int b, int e, int i, int j, long long x)
+void update(int n, int b, int e, int i, int j, ll x) // i and j constant
 {
 	if(i > e || j < b)
 	{
 		return;
 	}
-	if(b >=i && e <= j)
+	if(b >= i && e <= j)
 	{
 		tree[n].sum += (e - b + 1) * x;
 		tree[n].prop += x;
@@ -51,7 +51,7 @@ void update(int n, int b, int e, int i, int j, long long x)
 	tree[n].sum = tree[l].sum + tree[r].sum + (e - b + 1) * tree[n].prop;
 }
 
-long long query(int n, int b, int e, int i, int j, long long carry = 0)
+ll query(int n, int b, int e, int i, int j, ll carry = 0)
 {
 	if(i > e || j < b)
 	{
@@ -66,8 +66,8 @@ long long query(int n, int b, int e, int i, int j, long long carry = 0)
 	int r = 2*n + 1;
 	int mid = (b + e) / 2;
 
-	long long p1 = query(l, b, mid, i, j, carry + tree[n].prop);
-	long long p2 = query(r, mid+1, e, i, j, carry + tree[n].prop);
+	ll p1 = query(l, b, mid, i, j, carry + tree[n].prop);
+	ll p2 = query(r, mid+1, e, i, j, carry + tree[n].prop);
 
 	return p1 + p2;
 }
@@ -82,18 +82,20 @@ int main() {
 	{
 		int op;
 		cin >> op;
+
 		if(op == 1)
 		{
 			int i, j, x;
 			cin >> i >> j >> x;
 			update(1, 1, n, i+1, j, x);
 		}
-		else
+		else if(op == 2)
 		{
 			int i, j;
 			cin >> i >> j;
 			cout << query(1, 1, n, i+1, j) << endl;
 		}
 	}
+
 	return 0;
 }
